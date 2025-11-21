58a20f2bbce9f810112f26650b61964da125ca69
lwshakib <leadwithshakib@gmail.com>
2025-11-21 17:57:39 +0600

    Add API entry point files for server; includes TypeScript and JavaScript definitions, source maps, and exports the main app from src.

---
+++ server/api/index.d.ts
@@ -0,0 +1,3 @@
+import app from "../src";
+export default app;
+//# sourceMappingURL=index.d.ts.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,9 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
+
+export default app;
